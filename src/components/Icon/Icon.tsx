9e682232ac5c1ac6e65760d4f2b4254fce4de795
Paula Rolim <rolim.paula1999@gmail.com>
2023-01-24 21:42:31 -0300

    feat: icon component

---
+++ src/components/Icon/Icon.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { IconName, IconProps } from './types';
+
+import arrowRight from '../../assets/icons/arrow-right.svg';
+import arrowRightAlt from '../../assets/icons/arrow-right-alt.svg';
+import arrowLeft from '../../assets/icons/arrow-left.svg';
+import menu from '../../assets/icons/menu.svg';
+import position from '../../assets/icons/position.svg';
+import compass from '../../assets/icons/compass.svg';
+import settings from '../../assets/icons/settings.svg';
+import location from '../../assets/icons/location.svg';
+import graph from '../../assets/icons/graph.svg';
+
+const options: Record<IconName, any> = {
+  'arrow-right': arrowRight,
+  'arrow-left': arrowLeft,
+  'arrow-right-alt': arrowRightAlt,
+  menu,
+  position,
+  settings,
+  compass,
+  location,
+  graph,
+};
+
+export function Icon({ name }: IconProps) {
+  const Component = options[name];
+
+  return <Component />;
+}
---
+++ src/components/Icon/types.ts
@@ -0,0 +1,5 @@
+export type IconName = 'arrow-right' | 'arrow-left' | 'arrow-right-alt' | 'menu' | 'position' | 'settings' | 'compass' | 'location' | 'graph'
+
+export interface IconProps {
+  name: IconName;
+}
