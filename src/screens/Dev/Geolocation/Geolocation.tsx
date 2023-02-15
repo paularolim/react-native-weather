94f67e9551dcff6ec6b722cc8c5d0bf5f90859e5
Paula Rolim <rolim.paula1999@gmail.com>
2023-02-15 19:07:04 -0300

    chore: dev geolocation screen

---
+++ src/screens/Dev/Geolocation/Geolocation.tsx
@@ -0,0 +1,21 @@
+import { Text, TouchableOpacity, View } from 'react-native';
+import React, { useState } from 'react';
+import { GeolocationProps, getCurrentPosition } from '../../../services/Geolocation';
+
+export function DevGeolocation() {
+  const [location, setLocation] = useState<GeolocationProps | null>(null);
+
+  const handleLocation = async () => {
+    const position = await getCurrentPosition();
+    setLocation(position);
+  };
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>{JSON.stringify(location)}</Text>
+      <TouchableOpacity style={{ backgroundColor: 'blue' }} onPress={handleLocation}>
+        <Text style={{ color: 'white' }}>Location</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
