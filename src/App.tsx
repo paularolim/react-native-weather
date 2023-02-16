2bb72bb81dff4bd3009b4cb1aaf5b38560541163
Paula Rolim <rolim.paula1999@gmail.com>
2023-02-15 22:04:18 -0300

    chore: config moment

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import moment from 'moment';
+import 'moment/locale/pt-br';
+
 import { Home } from './screens/Home';
+
+moment.locale('pt-br');
 
 export function App() {
   return <Home />;
