123d2934311068efd9c35e88512058243a2b3a6b
Paula Rolim <rolim.paula1999@gmail.com>
2023-02-21 18:05:30 -0300

    fix: android permission

--- src/services/Geolocation/Geolocation.ts
+++ src/services/Geolocation/Geolocation.ts
@@ -1,7 +1,17 @@
+import { PermissionsAndroid, Platform } from 'react-native';
 import Geolocation from 'react-native-get-location';
 import { GeolocationProps } from './types';
 
 export async function getCurrentPosition(): Promise<GeolocationProps | null> {
+  if (Platform.OS === 'android') {
+    const hasPermission = await PermissionsAndroid.check(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+    );
+    if (!hasPermission) {
+      PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION);
+    }
+  }
+
   const location = await Geolocation.getCurrentPosition({
     enableHighAccuracy: false,
     timeout: 2000,
