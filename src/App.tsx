52c9d9cf3c0bf1d7dbcdb70a399ce7788d180ab5
Paula Rolim <rolim.paula1999@gmail.com>
2023-02-21 19:53:54 -0300

    feat: configure navigation

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import moment from 'moment';
 import 'moment/locale/pt-br';
 
-import { Home } from './screens/Home';
+import { AppRoutes } from './routes/AppRoutes';
 
 moment.locale('pt-br');
 
 export function App() {
-  return <Home />;
+  return <AppRoutes />;
 }
---
+++ src/routes/AppRoutes.tsx
@@ -0,0 +1,16 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+import { Home } from '../screens/Home';
+
+const Stack = createNativeStackNavigator();
+
+export function AppRoutes() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
