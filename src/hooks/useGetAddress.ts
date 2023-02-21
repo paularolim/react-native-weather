b452ab6deb0ac366dda491de35c0f56717c84af2
Paula Rolim <rolim.paula1999@gmail.com>
2023-02-21 18:47:16 -0300

    fix: useGetAddress

--- src/hooks/useGetAddress.ts
+++ src/hooks/useGetAddress.ts
@@ -1,38 +1,44 @@
 /* eslint-disable no-console */
 import axios, { AxiosInstance } from 'axios';
 import { useCallback, useState } from 'react';
+import { LOCATIONIQ_API_KEY } from '@env';
 import { GeolocationProps } from '../services/Geolocation';
 
-const baseURL = 'https://services.gisgraphy.com/reversegeocoding';
-const httpClient: AxiosInstance = axios.create({ baseURL });
+const apikey = LOCATIONIQ_API_KEY;
+const baseURL = 'https://us1.locationiq.com/v1/reverse';
+const httpClient: AxiosInstance = axios.create({ baseURL, params: { key: apikey } });
 
 interface Result {
   address: string
   loadingAddress: boolean
+  errorAddress: string
   getAddress: (position: GeolocationProps | null) => void
 }
 
 export function useGetAddress(): Result {
   const [address, setAddress] = useState('');
   const [loadingAddress, setLoadingAddress] = useState(true);
+  const [errorAddress, setErrorAddress] = useState('');
 
   const getAddress = useCallback((position: GeolocationProps | null) => {
     if (position && position.latitude && position.longitude) {
-      const url = `/search?&lat=${position.latitude}&lng=${position.longitude}&format=json`;
       httpClient
-        .get(url)
+        .get('', { headers: { 'Content-Type': 'application/json', Accept: 'application/json' }, params: { lat: position.latitude, lon: position.longitude, format: 'json' } })
         .then(({ data }) => {
-          if (data.result[0].city && data.result[0].state) {
-            setAddress(`${data.result[0].city},\n${data.result[0].state}`);
+          console.log(data);
+          if (data.address) {
+            const city = data.address?.city || data.address?.town || '';
+            setAddress(`${city},\n${data.address.state}`);
             setLoadingAddress(false);
           }
         })
         .catch((error) => {
           console.log(error);
           setLoadingAddress(false);
+          setErrorAddress(error?.message);
         });
     }
   }, []);
 
-  return { address, loadingAddress, getAddress };
+  return { address, loadingAddress, errorAddress, getAddress };
 }
