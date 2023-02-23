63b8167be3c4cd3565e4ad2fdeccbf90404ef6ab
Paula Rolim <rolim.paula1999@gmail.com>
2023-02-22 21:04:54 -0300

    feat: improve location

--- src/screens/Home/components/Location/Location.tsx
+++ src/screens/Home/components/Location/Location.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react';
-import { Image, Text, View } from 'react-native';
+import { Image, View } from 'react-native';
 import moment from 'moment';
 
 import mapImg from '../../../../assets/images/map.png';
-import { DateText, Place } from './styles';
+import { DateText, LoadingContainer, LoadingDataContainer, Place } from './styles';
 import { useGetPosition } from '../../../../hooks/useGetPosition';
 import { useGetAddress } from '../../../../hooks/useGetAddress';
+import { Shimmer } from '../../../../components/Shimmer';
 
 const date = moment(new Date()).format('dddd, DD/MMMM').replace('/', ' de ');
 
@@ -26,29 +27,36 @@ export function Location() {
       <View
         style={{
           paddingHorizontal: 24,
-          flexDirection: 'column',
+          flexDirection: 'row',
           justifyContent: 'space-between',
           alignItems: 'center',
+          marginVertical: 24,
         }}
       >
-        <Text>Erro :(</Text>
-        <Text>{errorAddress}</Text>
+        <View>
+          <Place>
+            Ops!
+            {'\n'}
+            Houve um erro.
+          </Place>
+          <DateText>Tente novamente mais tarde :(</DateText>
+        </View>
+        <Image source={mapImg} style={{ width: 60, height: 70, resizeMode: 'contain' }} />
       </View>
     );
   }
 
   if (loadingPosition || loadingAddress) {
     return (
-      <View
-        style={{
-          paddingHorizontal: 24,
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-        }}
-      >
-        <Text>Carregando...</Text>
-      </View>
+      <LoadingContainer>
+        <LoadingDataContainer>
+          <Shimmer height={27} width={83} radius={4} />
+          <Shimmer height={27} width={121} radius={4} />
+          <Shimmer height={18} width={144} radius={4} />
+        </LoadingDataContainer>
+
+        <Shimmer height={82} width={58} radius={4} />
+      </LoadingContainer>
     );
   }
 
@@ -59,6 +67,7 @@ export function Location() {
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
+        marginVertical: 24,
       }}
     >
       <View>
--- src/screens/Home/components/Location/styles.ts
+++ src/screens/Home/components/Location/styles.ts
@@ -1,4 +1,4 @@
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import styled from 'styled-components/native';
 
 export const Place = styled(Text)`
@@ -12,4 +12,16 @@ export const DateText = styled(Text)`
   font-size: 12px;
   line-height: 18px;
   color: #8D8D8D;
+`;
+
+export const LoadingContainer = styled(View)`
+  padding: 0px 24px;
+  height: 82px;
+  flex-direction: row;
+  justify-content: space-between;
+  margin: 32px 0px;
+`;
+
+export const LoadingDataContainer = styled(View)`
+  justify-content: space-between;
 `;
