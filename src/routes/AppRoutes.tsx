5c56fc734440472410360196e9a7492f26ded387
Paula Rolim <rolim.paula1999@gmail.com>
2023-03-31 19:04:27 -0300

    feat: add drawer menu component do drawer

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,15 +1,18 @@
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Home } from '../screens/Home';
+import { DrawerMenu } from './components/DrawerMenu';
 import { DrawerParamList } from './types';
 
 const Drawer = createDrawerNavigator<DrawerParamList>();
 
 export function AppRoutes() {
+  const drawerContent = useCallback((props) => <DrawerMenu {...props} />, []);
+
   return (
     <NavigationContainer>
-      <Drawer.Navigator screenOptions={{ headerShown: false }}>
+      <Drawer.Navigator screenOptions={{ headerShown: false }} drawerContent={drawerContent}>
         <Drawer.Screen name="Home" component={Home} />
       </Drawer.Navigator>
     </NavigationContainer>
---
+++ src/routes/components/DrawerMenu/DrawerMenu.tsx
@@ -0,0 +1,53 @@
+import React, { useCallback } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { FlatList, ListRenderItemInfo, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Avatar } from '../../../components/Avatar';
+import { BottomImage, Container, Email, Middle, Name } from './styles';
+import { Button } from '../../../components/Button';
+import { DrawerItemProps } from './types';
+import { items } from './mock';
+
+import img from '../../../assets/images/moon-cloud-mid-rain.png';
+
+export function DrawerMenu() {
+  const { navigate } = useNavigation();
+
+  const Separator = useCallback(() => <View style={{ height: 24 }} />, []);
+
+  const Item = useCallback(
+    ({ item }: ListRenderItemInfo<DrawerItemProps>) => (
+      <Button.Container background="transparent" onPress={() => navigate(item.route as never)}>
+        <Button.Icon background="gradient" name={item.icon} />
+        <Button.Label fontWeight="bold">{item.label}</Button.Label>
+      </Button.Container>
+    ),
+    [],
+  );
+
+  return (
+    <Container colors={['#FFFFFF', '#806EF866']} start={{ x: 0.0, y: 0.5 }} end={{ x: 1, y: 0.5 }}>
+      <SafeAreaView>
+        <Avatar source={{ uri: 'https://static.diverseui.com/male-21.jpg' }} hasShadow />
+        <Name>Nome Sobrenome</Name>
+        <Email>exemplo@email.com</Email>
+
+        <Middle>
+          <FlatList data={items} ItemSeparatorComponent={Separator} renderItem={Item} />
+        </Middle>
+
+        <Button.Container
+          background="gradient"
+          hasShadow
+          onPress={() => navigate('Settings' as never)}
+        >
+          <Button.Icon name="settings" />
+          <Button.Label color="white" fontWeight="bold" fontSize={14}>
+            Configurações
+          </Button.Label>
+        </Button.Container>
+      </SafeAreaView>
+      <BottomImage source={img} />
+    </Container>
+  );
+}
