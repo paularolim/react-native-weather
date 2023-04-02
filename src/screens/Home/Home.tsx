b2a63d23ac0380ef5b3a149d9e4d35e201b34b4b
Paula Rolim <rolim.paula1999@gmail.com>
2023-04-02 17:35:27 -0300

    refactor: change home background

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
-import { SafeAreaView, View } from 'react-native';
+import { View } from 'react-native';
+import { useTheme } from 'styled-components';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { Header } from './components/Header';
 import { CardDetails } from './components/CardDetails';
 import { Location } from './components/Location';
@@ -8,6 +10,8 @@ import { HomeProps } from './types';
 import { HourDetails } from './components/HourDetails';
 
 export function Home({ navigation }: HomeProps) {
+  const theme = useTheme();
+
   const { getPosition, position, loadingPosition } = useGetPosition();
 
   const toggleDrawer = () => {
@@ -17,7 +21,7 @@ export function Home({ navigation }: HomeProps) {
   useEffect(getPosition, [getPosition]);
 
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: theme.colors.background }}>
       <View style={{ justifyContent: 'space-between', flex: 1 }}>
         <Header toggleDrawer={toggleDrawer} />
         <Location position={position} loadingPosition={loadingPosition} />
