6f0100e96a56b0c0a49eecf93af450e0e28d1e2b
Paula Rolim <rolim.paula1999@gmail.com>
2023-04-02 18:41:59 -0300

    chore: organize declarations

---
+++ src/@types/env.d.ts
@@ -0,0 +1,4 @@
+declare module '@env' {
+  export const LOCATIONIQ_API_KEY: string;
+  export const OPEN_WEATHER_API_KEY: string;
+}
--- src/@types/svg.d.ts
+++ src/@types/svg.d.ts
@@ -1,14 +1,7 @@
-declare module '*.png';
-
 declare module '*.svg' {
   import React from 'react';
   import { SvgProps } from 'react-native-svg';
 
   const content: React.FC<SvgProps>;
   export default content;
-}
-
-declare module '@env' {
-  export const LOCATIONIQ_API_KEY: string;
-  export const OPEN_WEATHER_API_KEY: string;
 }
