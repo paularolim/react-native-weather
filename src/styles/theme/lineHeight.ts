9ea05150d318d8a21e78ff099de7528fbdbdf8a2
Paula Rolim <rolim.paula1999@gmail.com>
2023-04-02 16:23:07 -0300

    chore: add font size and line height to theme

---
+++ src/styles/theme/lineHeight.ts
@@ -0,0 +1,16 @@
+export const lineHeight = {
+  /** 16px */
+  xs: 16,
+  /** 20px */
+  sm: 20,
+  /** 24px */
+  base: 24,
+  /** 28px */
+  lg: 28,
+  /** 28px */
+  xl: 28,
+  /** 32px */
+  '2xl': 32,
+  /** 60px */
+  '6xl': 60,
+};
--- src/styles/theme/theme.ts
+++ src/styles/theme/theme.ts
@@ -2,17 +2,24 @@ import { avatar } from './avatar';
 import { radius } from './radius';
 import { darkTheme } from './darkTheme';
 import { lightTheme } from './lightTheme';
+import { fontSize } from './fontSize';
+import { lineHeight } from './lineHeight';
+
+const commonTheme = {
+  avatar,
+  radius,
+  fontSize,
+  lineHeight,
+};
 
 export const theme = {
   light: {
     colors: lightTheme,
-    avatar,
-    radius,
+    ...commonTheme,
   },
   dark: {
     colors: darkTheme,
-    avatar,
-    radius,
+    ...commonTheme,
   },
 };
 
